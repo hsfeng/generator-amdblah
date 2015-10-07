1a15f559c92633cd466b311c4f003ee045d74aca
hsfeng <hsfeng@gmail.com>
2015-10-07 17:57:26 +0800

    Update to latest yeoman-generator

--- app/index.js
+++ app/index.js
@@ -2,28 +2,33 @@
 var util = require('util');
 var path = require('path');
 var yeoman = require('yeoman-generator');
+var wiring = require('html-wiring');
+var slugify = require("underscore.string/slugify");
 
 
 var AmdblahGenerator = module.exports = function AmdblahGenerator(args, options,
     config) {
     yeoman.generators.Base.apply(this, arguments);
-
+    /*
     this.on('end', function() {
         var cb = function(err) {
             console.log('hello error:' + err);
         };
+
         this.installDependencies({
             bower: false,
-            skipInstall: options['skip-install'],
+            skipMessage: options['skip-message'],
             callback: function() {
+                console.log('hello callback');
                 this.spawnCommand('grunt', [
                     'bower-install'
                 ]);
             }.bind(this)
         });
-    });
+    });*/
 
-    this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname,
+    this.pkg = JSON.parse(wiring.readFileAsString(path.join(
+        __dirname,
         '../package.json')));
 };
 
@@ -34,7 +39,7 @@ AmdblahGenerator.prototype.askFor = function askFor() {
 
     // have Yeoman greet the user.
     console.log(this.yeoman);
-    var appName = this._.slugify(this.appname);
+    var appName = slugify(this.appname);
 
     var prompts = [{
         name: 'appName',
@@ -43,20 +48,20 @@ AmdblahGenerator.prototype.askFor = function askFor() {
     }];
 
     this.prompt(prompts, function(props) {
-        this.appName = props.appName;
+        this.appName = slugify(props.appName);
 
         cb();
     }.bind(this));
 };
 
 AmdblahGenerator.prototype.app = function app() {
-    this.directory('app', 'app', true);
-    this.directory('test', 'test', true);
-    this.directory('web', 'web', true);
+    this.bulkDirectory('app', 'app', true);
+    this.bulkDirectory('test', 'test', true);
+    this.bulkDirectory('web', 'web', true);
 
     this.copy('app.js', 'app.js');
     this.copy('bower.json', 'bower.json');
-    this.copy('Gruntfile.js', 'Gruntfile.js');
+    this.bulkCopy('Gruntfile.js', 'Gruntfile.js');
     this.copy('package.json', 'package.json');
     this.copy('r.build.js', 'r.build.js');
 };
@@ -67,4 +72,16 @@ AmdblahGenerator.prototype.projectfiles = function projectfiles() {
     this.copy('_gitignore', '.gitignore');
     this.copy('_jshintrc', '.jshintrc');
 
+};
+
+AmdblahGenerator.prototype.install = function install() {
+    this.installDependencies({
+        bower: false,
+        callback: function() {
+            console.log('hello callback');
+            this.spawnCommand('grunt', [
+                'bower-install'
+            ]);
+        }.bind(this)
+    });
 };
